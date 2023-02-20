3dae51f5370b687af03a25068db4422d6dba9270
youngho_windows <hois1998@snu.ac.kr>
2023-02-20 21:58:49 +0900

    알림 보내고 구독해지 않습니다. 다시 구독하려하면 해지합니다.

--- expressBusSeatAlarm/js/notificationSW.js
+++ expressBusSeatAlarm/js/notificationSW.js
@@ -80,7 +80,7 @@ self.addEventListener('activate', async () => {
 // {success: true, message: {foundList: [{dprtTime: "14:50", busCmp: "동양고속", busGrade: "고속", remain: "5 석"}, {...}], time: {hours, minutes, seconds}, date: '12'}}
 // 실패 시, {success, message: 실패이유}
 // 메시지 시, {success: true/false, message}
-self.addEventListener('push', event => {
+self.addEventListener('push', async event => {
     console.log(`브라우저가 구독하고 있던 통고를 받은 상황`);
 
     if (event.data) {
@@ -127,12 +127,14 @@ self.addEventListener('push', event => {
             // }));
             event.waitUntil(self.registration.showNotification(`모든 스케줄 출발`, options));
 
-            self.clients.matchAll().then(clients => {
-                clients.forEach(client => {
-                    console.log(client);
-                    client.postMessage(`모든 스케줄 출발에 따른 알림등록버튼 생성`);
-                })
-            })  
+            let temp = await self.clients.matchAll();
+            console.log(temp);
+            // .then(clients => {
+            //     clients.forEach(client => {
+            //         console.log(client);
+            //         client.postMessage(`모든 스케줄 출발에 따른 알림등록버튼 생성`);
+            //     })
+            // })  
           
             // showLocalNotification('알림 종료', payload.message, self.registration)
             //     .then(_ => {
